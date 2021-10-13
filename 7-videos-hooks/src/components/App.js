3967209e66a693245800b0120061fc27e24eb79c
Grant Magdanz <grant@commonstock.com>
2021-10-13 13:16:53 -0400

    Custom hook

--- 7-videos-hooks/src/components/App.js
+++ 7-videos-hooks/src/components/App.js
@@ -1,27 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from './SearchBar'
 import VideoPlayer from './VideoPlayer'
 import VideoList from './VideoList'
-import youtube from '../api/youtube'
-
+import useVideos from '../hooks/useVideos'
 
 const App = () => {
-    const [videos, setVideos] = useState([])
+    const [videos, search] = useVideos('buildings')
     const [selectedVideo, setSelectedVideo] = useState(null)
 
-    const onSearchSubmit = async (term) => {
-        const response = await youtube.get('/search', {
-            params: {
-                q: term
-            }
-        })
-        setVideos(response.data.items)
-        setSelectedVideo(response.data.items[0])
-    }
+    useEffect(() => {
+        setSelectedVideo(videos[0])
+    }, [videos])
+
 
     return (
         <div className="ui container">
-            <SearchBar onSubmit={onSearchSubmit}/>
+            <SearchBar onSubmit={search}/>
             <div className="ui grid">
                 <div className="ui row">
                     <div className="eleven wide column">
