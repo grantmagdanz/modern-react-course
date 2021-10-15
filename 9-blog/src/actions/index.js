9d0fa9a4f72c77fa7e655d58e4a9f925ea7cc7aa
Grant Magdanz <grant@commonstock.com>
2021-10-15 16:31:22 -0400

    Finish blog app

--- 9-blog/src/actions/index.js
+++ 9-blog/src/actions/index.js
@@ -1,10 +1,30 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder'
+import _ from 'lodash';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value();
+}
 
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts');
 
     dispatch({
         type: 'FETCH_POSTS',
-        payload: response
+        payload: response.data
     })
 };
+
+export const fetchUser = id => async dispatch => {
+    const response = await jsonPlaceholder.get(`/users/${id}`)
+
+    dispatch({
+        type: 'FETCH_USER',
+        payload: response.data
+    })
+}
