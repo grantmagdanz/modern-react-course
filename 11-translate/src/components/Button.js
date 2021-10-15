9105ae62cc6cb530abce94c9196b47b0b05a054b
Grant Magdanz <grant@commonstock.com>
2021-10-15 12:04:46 -0400

    Finish translate app

---
+++ 11-translate/src/components/Button.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+
+class Button extends React.Component {
+    
+
+    render() {
+        return (
+            <ColorContext.Consumer>
+                {(color) => {
+                    return (
+                        <button className={`ui button ${color}`}>
+                            <LanguageContext.Consumer>
+                                {({ language }) =>  language === 'english' ? 'Submit' : 'Voorleggen'}
+                            </LanguageContext.Consumer>
+                        </button>
+                    )
+                }}
+            </ColorContext.Consumer>
+        )
+    }
+}
+
+export default Button;
